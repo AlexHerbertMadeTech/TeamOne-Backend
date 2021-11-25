3cc357719686eaad6e77be28ee18e7b1ca1ae2d6
James Francis <james.francis@madetech.com>
2021-11-25 11:28:02 +0000

    WIP websocket start

---
+++ index.js
@@ -0,0 +1,16 @@
+const WebSocket = require("ws");
+const uuidv4 = require("uuidv4");
+
+const wss = new WebSocket.Server({ port: 1428 });
+
+const clients = new Map();
+
+wss.on("connection", (ws) => {
+    const id = uuidv4();
+    const color = Math.floor(Math.random() * 360);
+    const metadata = { id, color };
+
+    clients.set(ws, metadata);
+
+    ws.on("message", (messageAsString) => {});
+});
