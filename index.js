99d843b5c022aa4679470b5ef48b7bc94f99bda3
Isabel Jenkins <isabel.jenkins@madetech.com>
2021-11-25 18:27:44 +0000

    Fixed double jump, modified jump speed to feel smoother

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 1428 });
 let gameLoopInterval;
 
-const worldFloor = 600;
+const worldFloor = 400;
+const worldEnd = 1200;
 const clients = new Map();
 var availableActions = ['jump', 'duck', 'shoot', 'spectate'];
-var player = {width: 100, height: 100, x: 100, y: 550, id: 123, type: 'player', jump: 0 };
+var player = {width: 50, height: 100, x: 100, y: worldFloor - 50, id: 123, type: 'player', jumpFrames: 0, jumping: false, jumpSpeed: 0};
 var entities = [];
 var score = 0;
+let gameSpeed = 10;
 
 class jumpObstacle {
     constructor(height, width, id) {
         this.height = height;
         this.width = width;
-        this.x = 800;
-        this.y = 600 - (height / 2 );
+        this.x = worldEnd + (width / 2);
+        this.y = worldFloor - (height / 2 );
         this.type = 'jumpObstacle';
         this.id = id;
     }
 
-    move() {
-        this.x--;
+    move(gameSpeed) {
+        this.x -= gameSpeed;
     }
     
     collide(entity) {
@@ -48,22 +50,21 @@ wss.on("connection", (ws) => {
 
     ws.on('message', (data) => {
         let message = JSON.parse(data);
-        console.log(message);
         processEvent(ws, message);
     });
 });
 
 function processEvent(ws, data) {
-    if (data.event == 'jump') {
-        player.jump = 10;
-        console.log("Jump!");
+    if (data.event == 'jump' && !player.jumping) {
+        player.jumpFrames = 12;
+        player.jumpSpeed = 12;
+        player.jumping = true;
     }
 }
 
 function generateClient(ws) {
     var clientId = Math.floor((Math.random() * 1000) + 1);
 
-    //TODO Deal with what happens if there's more than 4 people <- It's called spectators :p
     var clientDetails = {
         event: 'startup',
         id: clientId,
@@ -88,21 +89,16 @@ function calculateFrame() {
     if (spawn) {
         chanceToSpawn = 500;
         var id = Math.floor((Math.random() * 1000) + 1);
-        entities.push(new jumpObstacle(100, 100, id));
+        entities.push(new jumpObstacle(50, 50, id));
     } else {
         chanceToSpawn--;
     }
 
-    if (player.jump > 0) {
-        player.y--;
-        player.jump--;
-    } else if (player.y < worldFloor - (player.height / 2)) {
-        player.y++;
-    }
+    playerJumpLogic();
 
     let isDead = false;
     entities.forEach(entity => {
-        entity.move()
+        entity.move(gameSpeed)
         if (entity.collide(player)) {
             isDead = true;
         }
@@ -127,6 +123,20 @@ function calculateFrame() {
                 event: "death"
             }));
         });
+    }
+}
+
+function playerJumpLogic() {
+    if (player.jumpFrames > 0) {
+        player.y -= player.jumpSpeed;
+        player.jumpFrames--;
+        player.jumpSpeed--;
+    } else if (player.y < worldFloor - (player.height / 2)) {
+        player.y += player.jumpSpeed;
+        player.jumpSpeed++;
+    } else {
+        player.jumping = false;
+        player.jumpSpeed = 0;
     }
 }
 
