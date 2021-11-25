85435ec56b64b5646a2954c4c58781f77ef61def
Isabel Jenkins <isabel.jenkins@madetech.com>
2021-11-25 15:22:30 +0000

    Added hitboxes
    Co-authored-by: Alexandra Herbert <AlexHerbertMadeTech@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const WebSocket = require("ws");
-const uuidv4 = require("uuidv4");
 
 const wss = new WebSocket.Server({ port: 1428 });
+let gameLoopInterval;
 
 const clients = new Map();
-var player = {width: 100, height: 100, x: 100, y: 600, id: 123, type: 'player'};
+var player = {width: 100, height: 100, x: 100, y: 550, id: 123, type: 'player'};
 var entities = [];
 
 class jumpObstacle {
@@ -12,16 +12,27 @@ class jumpObstacle {
         this.height = height;
         this.width = width;
         this.x = 800;
-        this.y = 600;
+        this.y = 600 - (height / 2 );
+        this.type = 'jumpObstacle';
         this.id = id;
     }
 
     move() {
         this.x--;
     }
+    
+    collide(entity) {
+        let r1 = { left: this.x - this.width / 2, right: this.x + this.width / 2, top: this.y - this.height / 2, bottom: this.y + this.height / 2 }
+        let r2 = { left: entity.x - entity.width / 2, right: entity.x + entity.width / 2, top: entity.y - entity.height / 2, bottom: entity.y + entity.height / 2 }
+
+        return !(r2.left > r1.right || 
+            r2.right < r1.left || 
+            r2.top > r1.bottom ||
+            r2.bottom < r1.top);
+    }
 }
 
-var chanceToSpawn = 10000;
+var chanceToSpawn = 500;
 
 wss.on("connection", (ws) => {
     console.log('New connection')
@@ -50,6 +61,7 @@ function processMessage(ws, message) {
 function generateClient(ws) {
     var clientId = Math.floor((Math.random() * 1000) + 1);
     var availableActions = ['jump', 'duck', 'shoot'];
+
     [...clients.keys()].forEach(element => {
         if (element.action == 'jump'){
             availableActions = availableActions.filter(item => item !== 'jump')
@@ -61,34 +73,39 @@ function generateClient(ws) {
             availableActions = availableActions.filter(item => item !== 'shoot')
         }
     });
-    //TODO Deal with what happens if there's more than 4 people
+
+    //TODO Deal with what happens if there's more than 4 people <- It's called spectators :p
     var clientDetails = {
         event: 'startup',
         id: clientId,
         action: availableActions[0]
     }
+
     clients.set(ws, clientDetails)
 }
 
 function gameLoop() {
     interval = 1000 / 30;
-    setInterval(calculateFrame, interval);
-
-    
+    gameLoopInterval = setInterval(calculateFrame, interval);
 }
 
 function calculateFrame() {
     var spawn = Math.floor((Math.random() * chanceToSpawn) + 1) === 1;
-    if(spawn){
-        chanceToSpawn = 10000;
+    if (spawn) {
+        chanceToSpawn = 500;
         var id = Math.floor((Math.random() * 1000) + 1);
         entities.push(new jumpObstacle(100, 100, id));
-    }else{
-        chanceToSpawn -= 10;
-    };
+    } else {
+        chanceToSpawn--;
+    }
 
+    let isDead = false;
     entities.forEach(entity => {
         entity.move()
+        if (entity.collide(player)) {
+            isDead = true;
+            console.log("Is Dead");
+        }
     });
 
     const frame =  {
@@ -100,6 +117,15 @@ function calculateFrame() {
     [...clients.keys()].forEach(client => {
         client.send(JSON.stringify(frame))
     });
+
+    if (isDead) {
+        clearInterval(gameLoopInterval);
+        [...clients.keys()].forEach(client => {
+            client.send(JSON.stringify({
+                event: "death"
+            }));
+        });
+    }
 }
 
 console.log("Application started");
