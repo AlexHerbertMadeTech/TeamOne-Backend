2bebc8d86edbbe82244fb8c84c41ef06738cd917
James Francis <james.francis@madetech.com>
2021-11-25 14:29:03 +0000

    sending back player and entities
    Co-authored-by: Alexandra Herbert <AlexHerbertMadeTech@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const uuidv4 = require("uuidv4");
 const wss = new WebSocket.Server({ port: 1428 });
 
 const clients = new Map();
+var player = {width: 100, height: 100, x: 100, y: 600, id: 123, type: 'player'};
+var entities = [];
+
+class jumpObstacle {
+    constructor(height, width, id) {
+        this.height = height;
+        this.width = width;
+        this.x = 800;
+        this.y = 600;
+        this.id = id;
+    }
+
+    move() {
+        this.x--;
+    }
+}
+
+var chanceToSpawn = 1000;
 
 wss.on("connection", (ws) => {
     console.log('New connection')
@@ -11,6 +29,7 @@ wss.on("connection", (ws) => {
     generateClient(ws)
     ws.send(JSON.stringify(clients.get(ws)))
     
+    gameLoop();
     
     // const metadata ={ id: Math.random() * 360 };
     // clients.set(ws, metadata);
@@ -49,6 +68,38 @@ function generateClient(ws) {
         action: availableActions[0]
     }
     clients.set(ws, clientDetails)
+}
+
+function gameLoop() {
+    interval = 1000 / 30;
+    setInterval(calculateFrame(), interval);
+
+    
+}
+
+function calculateFrame() {
+    var spawn = Math.floor((Math.random() * chanceToSpawn) + 1) === 1;
+    if(spawn){
+        chanceToSpawn = 1000;
+        var id = Math.floor((Math.random() * 1000) + 1);
+        entities.push(new jumpObstacle(100, 100, id));
+    }else{
+        chanceToSpawn -= 10;
+    };
+
+    entities.forEach(entity => {
+        entity.move()
+    });
+
+    const frame =  {
+        event: gameUpdate,
+        player: player,
+        obstacles: entities
+    };
+
+    [...clients.keys()].forEach(client => {
+        client.send(frame)
+    });
 }
 
 console.log("Application started");
